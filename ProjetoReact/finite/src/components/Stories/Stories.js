2e68bef1d3b2bb219a0524533d3e739cd94f87f1
Andressa H Cardoso <andressahenselcardoso@gmail.com>
2023-11-19 21:55:53 -0300

    Organizando código - Stories

--- ProjetoReact/finite/src/components/Stories/Stories.js
+++ ProjetoReact/finite/src/components/Stories/Stories.js
@@ -1,62 +1,45 @@
 import { StorieSection, Storie, Div, StorieImgDiv, StorieImg, TextDiv, Profile, Account, ImgDate, DateText, Month } from "./Stories.jsx";
 
-// React Router
+// React
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 // Components
 import MainHeader from "../MainHeader/MainHeader";
 import Nav from "../Nav/Nav";
 
-// Images
-import storie1 from '../../assets/Storie/storie1.png'
-import storie2 from '../../assets/Storie/storie2.png'
-import storie3 from '../../assets/Storie/storie3.png'
-import storie4 from '../../assets/Storie/storie4.png'
-import storie5 from '../../assets/Storie/storie5.png'
-import user1 from '../../assets/user1.svg'
-import user2 from '../../assets/user2.svg'
-import user3 from '../../assets/user3.svg'
-import user4 from '../../assets/user4.svg'
-
 // Icons
 import DateIcon from '../../assets/Icons/line.svg'
-
 
 // Api
 import { api } from "../../services/api";
 
-
-import { useUserContext } from './UserContext.js';
-import { useEffect, useState } from "react";
-
+// Theme
 import { useTheme } from "../../hooks/useTheme";
 
+
 function Stories() {
-    const {theme} = useTheme();
-
     const navigate = useNavigate();
-
-    const images = 'http://localhost:3001/uploads/';
-
-    const [data, setData] = useState([]);
-    // const { setUserData } = useUserContext();
-
-    // function goToUserStorie(personVar, accountVar, userVar, storieVar, nextVar) {
-    //     const userData = { person: personVar, account: accountVar, user: userVar, imagem: storieVar, next: nextVar }; // Substitua pelos dados que deseja passar
-    //     console.log('userData :', userData);
-    //     // setUserData(userData);
-    //     navigate(`/User/Storie`);
-    // }
-
+    
     // Navigate functions
     function goToUserStorie(id) {
         navigate(`/User/Storie/${id}`);
     }
+    
+    
+    // Hooks
+    const [data, setData] = useState([]);
 
+    const images = 'http://localhost:3001/uploads/';
+
+
+    // Theme definition
+    const {theme} = useTheme();
+
+
+    // UseEffect Onload
     useEffect(() => {
-        
-  
-          const fetchData = async () => {
+        const fetchData = async () => {
               try {
                   const response = await api.get('/getStories');
                   setData(response.data)
